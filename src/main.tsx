67ee0ebcf853869452e1edd5ec6cac83d76e3c4f
Fazrin <fazrin.mutaqin@wgs-id.com>
2022-12-13 10:59:48 +0700

    Install & Setup React Query

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,16 @@ import App from './App';
 import './index.css';
 import store from './store/store';
 import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>
 );
